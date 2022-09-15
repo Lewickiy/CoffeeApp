067f3f43fbfc2b97545db8b1ad2e20e5fde804dd
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-09-15 20:26:42 +0300

    Изменение наименований переменных

--- src/main/java/com/lewickiy/coffeeboardapp/database/currentSale/SaleProduct.java
+++ src/main/java/com/lewickiy/coffeeboardapp/database/currentSale/SaleProduct.java
@@ -1,70 +1,85 @@
 package com.lewickiy.coffeeboardapp.database.currentSale;
 
+import com.lewickiy.coffeeboardapp.database.DatabaseConnector;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class SaleProduct {
-    private int saleProdId;
-    private int prodSaleId;
-    private String prodName;
-    private double priceProdSale;
-    private int amountProdSale;
-    private double sumProdSale;
+    private int saleId;
+    private int productId;
+    private String product;
+    private double price;
+    private int discountId;
+    private int amount;
+    private double sum;
 
     public SaleProduct(int saleProdId, int prodSaleId, String prodName, double priceProdSale, int amountProdSale, double sumProdSale) {
-        this.saleProdId = saleProdId;
-        this.prodSaleId = prodSaleId;
-        this.prodName = prodName;
-        this.priceProdSale = priceProdSale;
-        this.amountProdSale = amountProdSale;
-        this.sumProdSale = sumProdSale;
+        this.saleId = saleProdId;
+        this.productId = prodSaleId;
+        this.product = prodName;
+        this.price = priceProdSale;
+        this.amount = amountProdSale;
+        this.sum = sumProdSale;
     }
 
     public SaleProduct(int prodSaleId, String prodName, double priceProdSale) {
-        this.prodSaleId = prodSaleId;
-        this.prodName = prodName;
-        this.priceProdSale = priceProdSale;
+        this.productId = prodSaleId;
+        this.product = prodName;
+        this.price = priceProdSale;
     }
 
-    public int getSaleProdId() {
-        return saleProdId;
+    public int getSaleId() {
+        return saleId;
     }
 
-    public void setSaleProdId(int saleProdId) {
-        this.saleProdId = saleProdId;
+    public void setSaleId(int saleId) {
+        this.saleId = saleId;
     }
 
-    public int getProdSaleId() {
-        return prodSaleId;
+    public int getProductId() {
+        return productId;
     }
 
-    public void setProdSaleId(int prodSaleId) {
-        this.prodSaleId = prodSaleId;
+    public void setProductId(int productId) {
+        this.productId = productId;
     }
 
-    public String getProdName() {
-        return prodName;
+    public String getProduct() {
+        return product;
     }
 
-    public void setProdName(String prodName) {
-        this.prodName = prodName;
+    public void setProduct(String product) {
+        this.product = product;
     }
 
-    public double getPriceProdSale() {
-        return priceProdSale;
+    public double getPrice() {
+        return price;
     }
 
-    public void setPriceProdSale(double priceProdSale) {
-        this.priceProdSale = priceProdSale;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
-    public int getAmountProdSale() {
-        return amountProdSale;
+    public int getDiscountId() {
+        return discountId;
     }
 
-    public void setAmountProdSale(int amountProdSale) {
-        this.amountProdSale = amountProdSale;
+    public void setDiscountId(int discountId) {
+        this.discountId = discountId;
     }
 
-    public double getSumProdSale() {
-        return sumProdSale;
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getSum() {
+        return sum;
     }
 
     public void setSumProdSale(double sumProdSale) {
