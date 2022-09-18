23e63a56e71940401a54283d09ab8d64a7991af2
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-09-18 17:16:34 +0300

    в SaleProduct.java добавлена переменная discount (размер скидки). Изменения включают операции со скидкой.

--- src/main/java/com/lewickiy/coffeeboardapp/database/currentSale/SaleProduct.java
+++ src/main/java/com/lewickiy/coffeeboardapp/database/currentSale/SaleProduct.java
@@ -12,14 +12,17 @@ public class SaleProduct {
     private String product;
     private double price;
     private int discountId;
+    private int discount;
     private int amount;
     private double sum;
 
-    public SaleProduct(int saleProdId, int prodSaleId, String prodName, double priceProdSale, int amountProdSale, double sumProdSale) {
+    public SaleProduct(int saleProdId, int prodSaleId, String prodName, double priceProdSale, int discountId, int discount, int amountProdSale, double sumProdSale) {
         this.saleId = saleProdId;
         this.productId = prodSaleId;
         this.product = prodName;
         this.price = priceProdSale;
+        this.discountId = discountId;
+        this.discount = discount;
         this.amount = amountProdSale;
         this.sum = sumProdSale;
     }
@@ -70,6 +73,14 @@ public class SaleProduct {
         this.discountId = discountId;
     }
 
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
     public int getAmount() {
         return amount;
     }
@@ -88,6 +99,9 @@ public class SaleProduct {
     public static void addProductsToSale(ArrayList<SaleProduct> currentSaleProducts, CurrentSale currentSale) throws SQLException {
         Statement statement = DatabaseConnector.getConnection().createStatement();
         for (SaleProduct currentSaleProduct : currentSaleProducts) {
+            if (currentSaleProduct.getDiscountId() == 0) {
+                currentSaleProduct.setDiscountId(1);
+            }
             statement.executeUpdate("INSERT sale_product("
                 + "sale_id, "
                 + "product_id, "
@@ -97,7 +111,7 @@ public class SaleProduct {
                 + "sum) VALUES ('"
                 + currentSale.getSaleId() + "', '"
                 + currentSaleProduct.getProductId() + "', '"
-                + 10 + "', '"
+                + currentSaleProduct.getDiscountId() + "', '"
                 + currentSaleProduct.getPrice() + "', '"
                 + currentSaleProduct.getAmount() + "', '"
                 + currentSaleProduct.getSum()  + "')");
