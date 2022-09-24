ec9329fe25971393c63b0de395b7a671bd3d920b
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-09-24 16:30:24 +0300

    Добавлен комментарий-документации

--- src/main/java/com/lewickiy/coffeeboardapp/database/Query.java
+++ src/main/java/com/lewickiy/coffeeboardapp/database/Query.java
@@ -7,6 +7,12 @@ import java.sql.Statement;
 import static com.lewickiy.coffeeboardapp.database.DatabaseConnector.getConnection;
 
 public class Query {
+    /**
+     * Запрос SELECT FROM к таблице sql.
+     * @param tableName - принимаемый параметр типа String - Имя таблицы, к которой обращаемся.
+     * @return - метод возвращает объект resultSet класса ResultSet.
+     * @throws SQLException
+     */
     public static ResultSet selectFromSql(String tableName) throws SQLException {
         Statement statement = getConnection().createStatement(); //создаётся подключение
         String query = "SELECT * FROM " + tableName; //создаётся запрос к базе данных
