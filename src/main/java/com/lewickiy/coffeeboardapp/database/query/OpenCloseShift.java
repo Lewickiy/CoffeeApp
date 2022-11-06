ed08249563a6a9d2603ee56c7b61c3080618b119
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-11-07 01:39:59 +0300

    Класс открытия/закрытия смены

---
+++ src/main/java/com/lewickiy/coffeeboardapp/database/query/OpenCloseShift.java
@@ -0,0 +1,28 @@
+package com.lewickiy.coffeeboardapp.database.query;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static com.lewickiy.coffeeboardapp.database.DatabaseConnector.getConnection;
+import static com.lewickiy.coffeeboardapp.database.outlet.Outlet.currentOutlet;
+
+public class OpenCloseShift {
+    public static void updateShiftSql(boolean sql) throws SQLException {
+        Connection con = getConnection("local_database");
+        int sqlInt = 0;
+
+        if (sql == true) {
+            sqlInt = 1;
+        }
+
+        String query = "UPDATE shift SET is_closed = "
+                + sqlInt
+                + " WHERE outlet_id = "
+                + currentOutlet.getOutletId() + ";";
+        Statement statement = con.createStatement();
+        statement.executeUpdate(query);
+        statement.close();
+        con.close();
+    }
+}
