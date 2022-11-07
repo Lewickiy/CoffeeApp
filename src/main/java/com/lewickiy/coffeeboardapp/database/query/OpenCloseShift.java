bced216657264a3d10db8d6950d97bfe84a523ad
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-11-07 15:20:06 +0300

    Добавлено внесение наличных для размена в базу данных при открытии смены

--- src/main/java/com/lewickiy/coffeeboardapp/database/query/OpenCloseShift.java
+++ src/main/java/com/lewickiy/coffeeboardapp/database/query/OpenCloseShift.java
@@ -8,20 +8,21 @@ import static com.lewickiy.coffeeboardapp.database.DatabaseConnector.getConnecti
 import static com.lewickiy.coffeeboardapp.database.outlet.Outlet.currentOutlet;
 
 public class OpenCloseShift {
-    public static void updateShiftSql(boolean sql) throws SQLException {
+    public static void updateShiftSql(boolean sql, double cashDeposit) throws SQLException {
         Connection con = getConnection("local_database");
         int sqlInt = 0;
 
         if (sql) {
             sqlInt = 1;
         }
-
         String query = "UPDATE shift SET is_closed = "
-                + sqlInt
+                + sqlInt + ", cash_deposit = " + cashDeposit
                 + " WHERE outlet_id = "
                 + currentOutlet.getOutletId() + ";";
+
         Statement statement = con.createStatement();
         statement.executeUpdate(query);
+
         statement.close();
         con.close();
     }
