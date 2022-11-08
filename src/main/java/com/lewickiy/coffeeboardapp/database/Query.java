07036c927a18827847dbe75bdf8171ab75ef2b69
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-11-09 00:16:23 +0300

    Update Query.java
    ...

--- src/main/java/com/lewickiy/coffeeboardapp/database/Query.java
+++ src/main/java/com/lewickiy/coffeeboardapp/database/Query.java
@@ -13,39 +13,32 @@ public class Query {
      * @throws SQLException - TODO проработать, описать.
      */
     public static ResultSet selectAllFromSql(Connection con, String dbName, String tableName) throws SQLException {
-        System.out.println("Start connection to " + dbName + " table: " + tableName);
         Statement statement = con.createStatement(); //создаётся подключение
-        System.out.println("Connected to " + dbName + " table: " + tableName);
         String query = null;
         if (dbName.equals("network_database")) {
             query = "SELECT * FROM " + tableName; //создаётся запрос к базе данных
         } else if (dbName.equals("local_database")) {
             query = "SELECT * FROM " + tableName + ";";
         }
-        System.out.println("Show all data from " + dbName);
         return statement.executeQuery(query);
     }
 
     public static void deleteFromSql(Connection con, String dbName, String tableName, String action) throws SQLException {
         action = action.toUpperCase();
         String query = action + " FROM " + tableName;
-        System.out.println("CONNECTION TO " + dbName + " for DELETING all data FROM " + tableName);
         Statement statement = con.createStatement(); //создаётся подключение
         statement.executeUpdate(query);
-        System.out.println("DATA from " + tableName + " DELETED");
         statement.close();
     }
 
     public static void showTablesFromSql(Connection con, String dbName, String tableName, String action) throws SQLException {
         action = action.toUpperCase();
         String query = action + " tables";
-        System.out.println("CONNECTION TO " + dbName + " for show all tables FROM DB");
         Statement statement = con.createStatement(); //создаётся подключение
         ResultSet rs = statement.executeQuery(query);
         while(rs.next()) {
-            System.out.print(rs.getString(1) + " | ");
+            //TODO
         }
-        System.out.println();
         statement.close();
     }
 
