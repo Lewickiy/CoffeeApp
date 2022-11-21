5a353c791eecd9b3a7d02c0247d249a7ae553a76
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-11-21 14:21:40 +0300

    схема подключения изменена

--- src/main/java/com/lewickiy/coffeeboardapp/database/DatabaseConnector.java
+++ src/main/java/com/lewickiy/coffeeboardapp/database/DatabaseConnector.java
@@ -1,31 +1,37 @@
 package com.lewickiy.coffeeboardapp.database;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class DatabaseConnector {
-
-    private static final String DB_DRIVER = "jdbc:mysql:";
-    private static final String HOST = "server5.hosting.reg.ru";
-    private static final int PORT = 3306;
-    private static final String DB_NAME = "u1718085_default";
-    private static final String URL = DB_DRIVER + "//" + HOST + ":" + PORT + "/" + DB_NAME;
-    private static final String USER = "u1718085_default";
-    private static final String PASSWORD = "6RnDd5Nkk1EQGjv3";
     /**
      * Метод реализует подключение к базе данных<br>
      * @return - возвращает Connection connectNow; при запросе из прочих участков кода
      */
     public static Connection getConnection(String dbName) throws SQLException { // метод подключения к базе данных
+        Properties props = new Properties();
+        try (InputStream in = Files.newInputStream(Paths.get("database.properties"))){
+            props.load(in);
+            String url = props.getProperty("url");
+            String username = props.getProperty("username");
+            String password = props.getProperty("password");
         Connection con = null;
         if (dbName.equals("network_database")) {
-            con = DriverManager.getConnection(URL, USER, PASSWORD);
+            con = DriverManager.getConnection(url, username, password);
         } else if (dbName.equals("local_database")) {
             con = DriverManager.getConnection("jdbc:sqlite:coffeeapp_local.db");
         }
         return con;
+    } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
