9f0fd2ff891baed16104a52cb16b0a315ad230a6
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-11-26 16:15:52 +0300

    Добавлен рабочий стол Администратора. На данный момент не наполнен функциональностью

--- src/main/java/com/lewickiy/coffeeboardapp/controllers/administrator/AdministratorController.java
+++ src/main/java/com/lewickiy/coffeeboardapp/controllers/administrator/AdministratorController.java
@@ -1,29 +1,32 @@
 package com.lewickiy.coffeeboardapp.controllers.administrator;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-import static com.lewickiy.coffeeboardapp.database.local.todaySales.TodaySalesList.*;
+import java.text.SimpleDateFormat;
 
 public class AdministratorController {
-    @FXML
-    private Label yesterdayAllLabel;
-    @FXML
-    private Label yesterdayCashLabel;
-    @FXML
-    private Label yesterdayCardLabel;
-    //_______________________________
-    @FXML
-    private Label todayAllLabel;
-    @FXML
-    private Label todayCashLabel;
-    @FXML
-    private Label todayCardLabel;
+
+    //TODO
 
     @FXML
+    private Label clockLabel;
+    @FXML
     void initialize() {
-        todayAllLabel.setText(String.valueOf(sumAll()));
-        todayCashLabel.setText(String.valueOf(sumCash()));
-        todayCardLabel.setText(String.valueOf(sumCard()));
+        Thread clockThread = new Thread(() -> {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                final String time = simpleDateFormat.format(new java.util.Date());
+                Platform.runLater(() -> clockLabel.setText(time));
+            }
+        });   clockThread.setDaemon(true);
+        clockThread.start();
+        //TODO
     }
 }
