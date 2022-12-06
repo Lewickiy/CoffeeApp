69903ddfa77813e250c9e8dfb47db02380b7385c
Anatoliy Lewickiy <96210003+Lewickiy@users.noreply.github.com>
2022-12-06 15:13:08 +0300

    Генератор идентификатора теперь генерирует информативный идентификатор типа long. Он включает в себя данные о дате, времени генерации, торговой точки и пользователе. В соответствии с этим произошло множество изменений в зависящих классах.

--- src/main/java/com/lewickiy/coffeeboardapp/idgenerator/UniqueIdGenerator.java
+++ src/main/java/com/lewickiy/coffeeboardapp/idgenerator/UniqueIdGenerator.java
@@ -1,15 +1,29 @@
 package com.lewickiy.coffeeboardapp.idgenerator;
 
-import java.util.UUID;
+import java.sql.Date;
+import java.sql.Time;
+
+import static com.lewickiy.coffeeboardapp.entities.outlet.Outlet.currentOutlet;
+import static com.lewickiy.coffeeboardapp.entities.user.UserList.currentUser;
 
 public class UniqueIdGenerator {
-    public static int getId() {
-        String id;
-        UUID uuid = UUID.randomUUID();
-        id = uuid.toString();
-        id = id.replace("-", "");
-        int num = id.hashCode();
-        num = num < 0 ? -num : num;
-        return num;
+    /**
+     * The method generates a receipt number in the following format:<br>
+     * 20221204 (date) + 145522 (time) + 5 (point of sale ID) + 2 (user ID)<br>
+     * = 2022120414552252.<br>
+     * @return - long receipt ID.
+     */
+    public static long getId() {
+        String date;
+        String time;
+        long resLong;
+        long nowDateLong = System.currentTimeMillis();
+        Date nowDate = new Date(nowDateLong);
+        long nowTimeLong = System.currentTimeMillis();
+        Time nowTime = new Time(nowTimeLong);
+        date = nowDate.toString().replace("-", "");
+        time = nowTime.toString().replace(":", "");
+        resLong = Long.parseLong(date + time + currentOutlet.getOutletId() + currentUser.getUserId());
+        return resLong;
     }
 }
